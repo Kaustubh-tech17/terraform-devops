1c3b0fd9baad733e12e2677dad4d14510480c173
Priya Salunkhe <priyagaikwad23@gmail.com>
2025-07-29 10:13:25 +0530

    main tf file

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
- 
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# Call the reusable S3 bucket module
+module "aws-s3-bucket" {
+  source = "./modules/aws_s3_bucket"
+
+  # Pass variables to the module
+  bucket_name = var.bucket_name
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+} 
