140427531c5dd6081d1b73086e67f6917c56cb3a
Priya Salunkhe <priyagaikwad23@gmail.com>
2025-07-29 10:16:11 +0530

    outputs tf file

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
- 
+output "s3_bucket_id" {
+  description = "The ID of the created S3 bucket."
+  value       = module.my_s3_bucket.bucket_id
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the created S3 bucket."
+  value       = module.my_s3_bucket.bucket_arn
+} 
