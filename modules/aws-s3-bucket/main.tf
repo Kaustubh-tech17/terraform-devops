b299ce46b6c4afaa279bae00c72da625d3770eee
Priya Salunkhe <priyagaikwad23@gmail.com>
2025-07-29 10:24:14 +0530

    main tf file

--- modules/aws-s3-bucket/main.tf
+++ modules/aws-s3-bucket/main.tf
@@ -1,1 +1,22 @@
- 
+# Create a private S3 bucket
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = var.tags
+}
+
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = var.versioning_status
+  }
+}
+
+# Block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+} 
