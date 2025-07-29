72af0822d3d3303bd6bdb3363ee4b024e7ad3e09
Priya Salunkhe <priyagaikwad23@gmail.com>
2025-07-29 10:18:15 +0530

    variables tf file

--- variables.tf
+++ variables.tf
@@ -1,1 +1,22 @@
- 
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+  default     = "s3-example"
+}
+
+variable "environment" {
+  description = "The deployment environment (e.g., dev, prod)."
+  type        = string
+  default     = "dev"
+}
+
+variable "bucket_name" {
+  description = "The name for the S3 bucket to be created."
+  type        = string
+} 
