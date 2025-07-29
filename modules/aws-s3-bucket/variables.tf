8ade8a2217d740c0afbdb308fed9587fe21149ce
Priya Salunkhe <priyagaikwad23@gmail.com>
2025-07-29 10:28:48 +0530

    variables tf file

--- modules/aws-s3-bucket/variables.tf
+++ modules/aws-s3-bucket/variables.tf
@@ -1,1 +1,16 @@
- 
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique."
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "versioning_status" {
+  description = "The versioning state of the S3 bucket. Can be 'Enabled', 'Disabled', or 'Suspended'."
+  type        = string
+  default     = "Enabled"
+} 
